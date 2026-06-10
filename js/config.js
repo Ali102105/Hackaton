99fa7c9483e73de7c9e5082eef29b6ff0303d43e
Ali102105 <alizuhy@outlook.com>
2026-06-10 14:16:12 +0200

    12345

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAMVIEC0lork20d7Z_Ma_6KxtgbgKKGE-k",
   authDomain: "hackaton-eba4d.firebaseapp.com",
+  databaseURL: "https://hackaton-eba4d-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "hackaton-eba4d",
   storageBucket: "hackaton-eba4d.firebasestorage.app",
   messagingSenderId: "837132896288",
